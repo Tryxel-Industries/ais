f66a81502f74d829423bf1c18fd2b5d5e1422371
trygve <git@woldseth.xyz>
2023-02-05 14:46:08 +0100

    initial

---
+++ src/simple_rr_ais.rs
@@ -0,0 +1,12 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::os::unix::raw::ino_t;
+use crate::enums::TrueFalseLabel;
+use crate::model::{EmbeddingElement, NewsArticle, SimpleDistance};
+
+
+struct SimpleRRAis {}
+
+impl SimpleRRAis {
+    pub fn train(articles: Vec<NewsArticle>) {}
+}
