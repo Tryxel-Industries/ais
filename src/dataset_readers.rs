f1c93fb1c215f5faa7986ad3a4f3dbdccd91dcb6
trygve <git@woldseth.xyz>
2023-04-13 12:15:14 +0200

    added reading of json and kaggle set support

--- src/dataset_readers.rs
+++ src/dataset_readers.rs
@@ -1,8 +1,13 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use json::JsonValue;
 
 use crate::representation::antigen::AntiGen;
+
+const REF_DATASET_DIR: &str = "./datasets/reference";
+const FAKE_NEWS_DATASET_DIR: &str = "./datasets/fake_news";
 
 fn normalize_features(feature_vec: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     let n_features = feature_vec.first().unwrap().len();
@@ -47,6 +52,15 @@ fn normalize_features(feature_vec: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     return ret;
 }
 
+fn normalize_features_ag(mut ag_vec: Vec<AntiGen>) -> Vec<AntiGen> {
+    let mut feature_vec: Vec<_> = ag_vec.iter().map(|ag| ag.values.clone()).collect();
+    let norm_features = normalize_features(feature_vec);
+    ag_vec.iter_mut().zip(norm_features).for_each(|(ag,norm_f)| ag.values = norm_f);
+    return ag_vec;
+
+}
+
+
 fn read_csv(path: &str) -> Vec<Vec<String>> {
     let mut ret_vec: Vec<Vec<String>> = Vec::new();
 
@@ -78,7 +92,7 @@ fn read_csv(path: &str) -> Vec<Vec<String>> {
 }
 
 pub fn read_iris() -> Vec<AntiGen> {
-    let mut data_vec = read_csv("./datasets/iris/iris.data");
+    let mut data_vec = read_csv(format!("{}/iris/iris.data", REF_DATASET_DIR).as_str());
 
     let (labels, dat): (Vec<String>, Vec<Vec<String>>) = data_vec
         .into_iter()
@@ -132,7 +146,7 @@ pub fn read_iris_snipped() -> Vec<AntiGen> {
 }
 
 pub fn read_wine() -> Vec<AntiGen> {
-    let mut data_vec = read_csv("./datasets/wine/wine.data");
+    let mut data_vec = read_csv(format!("{}/wine/wine.data", REF_DATASET_DIR).as_str());
 
     let (labels, dat): (Vec<String>, Vec<Vec<String>>) = data_vec
         .into_iter()
@@ -169,7 +183,7 @@ pub fn read_wine() -> Vec<AntiGen> {
 }
 
 pub fn read_diabetes() -> Vec<AntiGen> {
-    let mut data_vec = read_csv("./datasets/diabetes/diabetes.csv");
+    let mut data_vec = read_csv(format!("{}/diabetes/diabetes.csv", REF_DATASET_DIR).as_str());
 
     data_vec.remove(0);
     let (labels, dat): (Vec<String>, Vec<Vec<String>>) = data_vec
@@ -207,7 +221,7 @@ pub fn read_diabetes() -> Vec<AntiGen> {
 }
 
 pub fn read_sonar() -> Vec<AntiGen> {
-    let mut data_vec = read_csv("./datasets/sonar/sonar.all-data");
+    let mut data_vec = read_csv(format!("{}/sonar/sonar.all-data", REF_DATASET_DIR).as_str());
 
     let (labels, dat): (Vec<String>, Vec<Vec<String>>) = data_vec
         .into_iter()
@@ -249,7 +263,7 @@ pub fn read_sonar() -> Vec<AntiGen> {
 }
 
 pub fn read_glass() -> Vec<AntiGen> {
-    let mut data_vec = read_csv("./datasets/glass/glass.data");
+    let mut data_vec = read_csv(format!("{}/glass/glass.data", REF_DATASET_DIR).as_str());
 
     let (labels, dat): (Vec<String>, Vec<Vec<String>>) = data_vec
         .into_iter()
@@ -291,7 +305,7 @@ pub fn read_glass() -> Vec<AntiGen> {
 }
 
 pub fn read_ionosphere() -> Vec<AntiGen> {
-    let mut data_vec = read_csv("./datasets/ionosphere/ionosphere.data");
+    let mut data_vec = read_csv(format!("{}/ionosphere/ionosphere.data", REF_DATASET_DIR).as_str());
 
     let (labels, dat): (Vec<String>, Vec<Vec<String>>) = data_vec
         .into_iter()
@@ -336,7 +350,7 @@ pub fn read_ionosphere() -> Vec<AntiGen> {
 }
 
 pub fn read_pima_diabetes() -> Vec<AntiGen> {
-    let mut data_vec = read_csv("./datasets/pima_diabetes/pima_diabetes.csv");
+    let mut data_vec = read_csv(format!("{}/pima_diabetes/pima_diabetes.csv", REF_DATASET_DIR).as_str());
 
     data_vec.remove(0);
     let (labels, dat): (Vec<String>, Vec<Vec<String>>) = data_vec
@@ -384,7 +398,7 @@ pub fn read_pima_diabetes() -> Vec<AntiGen> {
 }
 
 pub fn read_spirals() -> Vec<AntiGen> {
-    let mut data_vec = read_csv("./datasets/spirals/spirals.csv");
+    let mut data_vec = read_csv(format!("{}/spirals/spirals.csv", REF_DATASET_DIR).as_str());
 
     let (labels, dat): (Vec<String>, Vec<Vec<String>>) = data_vec
         .into_iter()
@@ -428,4 +442,79 @@ pub fn read_spirals() -> Vec<AntiGen> {
         .collect();
 
     return antigens;
+}
+
+
+//
+//   Fake news
+//
+
+fn read_json(path: &str) -> JsonValue {
+    let mut lines: Vec<String> = Vec::new();
+
+    let f = File::open(path).unwrap();
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
+
+    loop {
+        let len = reader.read_line(&mut line).unwrap();
+
+        if line.ends_with("\n") {
+            line = line.strip_suffix("\n").unwrap().parse().unwrap();
+        }
+
+        if line.len() > 0 {
+            lines.push(line.clone());
+            line.clear();
+        } else {
+            break;
+        }
+    }
+    let json_str = lines.join("");
+    let js_obj = json::parse(&*json_str).unwrap();
+    return js_obj
+}
+
+pub fn read_kaggle_semantic() -> Vec<AntiGen> {
+    let path = format!("{}/kaggle/semantic_features_kaggle.json", FAKE_NEWS_DATASET_DIR);
+    let mut json_value = read_json(path.as_str());
+
+    json_value.entries().for_each(|(k,v)| println!("{:?}", k));
+
+    let mut news_entries: Vec<AntiGen> = Vec::new();
+
+
+    loop {
+        let mut val = json_value.pop();
+        if val.is_null(){
+            break;
+        }else {
+
+            let id = val.remove("id").as_i32().unwrap();
+            let title: String = val.remove("title").to_string();
+            let pub_date: String = val.remove("publishDate").dump();
+            let label : String = val.remove("label").to_string();
+            let mut res_map = val.remove("resultMap");
+
+            let mut feature_values: Vec<f64> = Vec::new();
+            res_map.entries_mut().for_each(|(k,v)|{
+                let f_val = v.remove("featureValue").as_f64().unwrap();
+                feature_values.push(f_val);
+            });
+
+            let ag =  AntiGen {
+                id: id as usize,
+                class_label: label.parse().unwrap(),
+                values: feature_values,
+            };
+            news_entries.push(ag)
+
+        }
+    }
+
+
+
+    news_entries = normalize_features_ag(news_entries);
+    return news_entries;
+
 }
